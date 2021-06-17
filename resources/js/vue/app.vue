8ec208d0c1461800b235c87ea739e7aab862b0c5
Dana Sugu <dana.sugu@gmail.com>
2021-06-17 16:17:42 +0300

    Update app.vue

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -4,16 +4,19 @@
           <h2 id="title">Todo list</h2>
           <add-item-form />
         </div>
+        <list-view />
         </div>
     </template>
 
 <script>
 import addItemForm from "./addItemForm"
 import listView from "./listView"
+import ListView from './listView.vue'
   export default {
     components: {
       addItemForm,
       listView
+        ListView
     }
   }
 </script scoped>
